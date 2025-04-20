7184949f8c62ddaeca9dc88e87059db8e45eceb2
vic123 <zhangzc_efz@163.com>
2025-04-20 17:30:42 +0800

    mips32 basic commands

--- mips32_cpu/MIPSsinglecycleprocessor/mipssingle.sv
+++ mips32_cpu/MIPSsinglecycleprocessor/mipssingle.sv
@@ -69,56 +69,57 @@ module controller(input  logic [5:0] op, funct,
                   output logic       jump,
                   output logic [2:0] alucontrol);
 
-  logic [1:0] aluop;
-  logic       branch;
-
+  logic [2:0] alucontrolIJ,alucontrolR;
+  logic [1:0] branch;
+  logic       isR;
+  mux2 #(3) aluc_mux(alucontrolIJ,alucontrolR,isR,alucontrol);
   maindec md(op, memtoreg, memwrite, branch,
-             alusrc, regdst, regwrite, jump, aluop);
-  aludec  ad(funct, aluop, alucontrol);
+             alusrc, regdst, regwrite, jump, alucontrolIJ,isR);
+  aludec  ad(funct, alucontrolR);
 
-  assign pcsrc = branch & zero;
+  assign pcsrc = (branch[1] & zero)|(branch[0] & !zero);
 endmodule
 
 module maindec(input  logic [5:0] op,
                output logic       memtoreg, memwrite,
-               output logic       branch, alusrc,
+               output logic [1:0] branch, 
+               output logic       alusrc,
                output logic       regdst, regwrite,
                output logic       jump,
-               output logic [1:0] aluop);
+               output logic [2:0] alucontrolIJ,
+               output logic       isR);
 
-  logic [8:0] controls;
+  logic [11:0] controls;
 
   assign {regwrite, regdst, alusrc, branch, memwrite,
-          memtoreg, jump, aluop} = controls;
+          memtoreg, jump,isR, alucontrolIJ} = controls;
 
   always_comb
     case(op)
-      6'b000000: controls <= 9'b110000010; // RTYPE
-      6'b100011: controls <= 9'b101001000; // LW
-      6'b101011: controls <= 9'b001010000; // SW
-      6'b000100: controls <= 9'b000100001; // BEQ
-      6'b001000: controls <= 9'b101000000; // ADDI
-      6'b000010: controls <= 9'b000000100; // J
-      default:   controls <= 9'bxxxxxxxxx; // illegal op
+      6'b000000: controls = 12'b110000001000; // RTYPE
+      6'b100011: controls = 12'b101000100010; // LW
+      6'b101011: controls = 12'b001001000010; // SW
+      6'b000100: controls = 12'b000100000110; // BEQ
+      6'b001000: controls = 12'b101000000010; // ADDI
+      6'b000010: controls = 12'b000000010000; // J
+      6'b001100: controls = 12'b101000000000; // ANDI
+      6'b001101: controls = 12'b101000000001; // ORI
+      6'b001010: controls = 12'b101000000111; // SLTI
+      6'b001101: controls = 12'b000010000110; // BNE
+      default:   controls = 12'bxxxxxxxxxxxx; // illegal op
     endcase
 endmodule
 
 module aludec(input  logic [5:0] funct,
-              input  logic [1:0] aluop,
-              output logic [2:0] alucontrol);
-
+              output logic [2:0] alucontrolR);
   always_comb
-    case(aluop)
-      2'b00: alucontrol <= 3'b010;  // add (for lw/sw/addi)
-      2'b01: alucontrol <= 3'b110;  // sub (for beq)
-      default: case(funct)          // R-type instructions
-          6'b100000: alucontrol <= 3'b010; // add
-          6'b100010: alucontrol <= 3'b110; // sub
-          6'b100100: alucontrol <= 3'b000; // and
-          6'b100101: alucontrol <= 3'b001; // or
-          6'b101010: alucontrol <= 3'b111; // slt
-          default:   alucontrol <= 3'bxxx; // ???
-        endcase
+    case(funct)          // R-type instructions
+      6'b100000: alucontrolR <= 3'b010; // add
+      6'b100010: alucontrolR <= 3'b110; // sub
+      6'b100100: alucontrolR <= 3'b000; // and
+      6'b100101: alucontrolR <= 3'b001; // or
+      6'b101010: alucontrolR <= 3'b111; // slt
+      default:   alucontrolR <= 3'bxxx; // ???
     endcase
 endmodule
 
